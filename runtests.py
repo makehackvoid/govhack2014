41c146fb39f63faaea09c11e8f84e4cc05749d2f
Jamie Reid <jamie@jre.id.au>
2014-07-12 09:33:49 +1000

    Fixed runtests.py

--- runtests.py
+++ runtests.py
@@ -1,10 +1,9 @@
-# import unittest
+import unittest
 import sys
 import os
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 if __name__ == "__main__":
-    # all_tests = unittest.TestLoader().discover('./tests')
-    # unittest.TextTestRunner().run(all_tests)
-    None
+    all_tests = unittest.TestLoader().discover('./tests')
+    unittest.TextTestRunner().run(all_tests)
