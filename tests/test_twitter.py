4593735acf371aa0334a688eeb4ad7938b82e452
Brenda Moon <brenda@moon.net.au>
2014-07-12 18:35:22 +1000

    first version of getting last suburb mentioned from twitter

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,5 +1,6 @@
 from twitter import Twitter
 import unittest
+import json
 
 
 class test_twitter(unittest.TestCase):
@@ -8,8 +9,16 @@ class test_twitter(unittest.TestCase):
         pass
 
     def test_index(self):
+        # need tests for oauth failure
         t = Twitter()
         t.get_tweet()
+        # only works when you know what the most recent tweet on twitter is
+        # self.assertEqual(t.get_tweet(), 'Charnwood', "Tweet found")
+        test = '[{"created_at":"Sat Jul 12 01:22:32 +0000 2014",'
+        test = test + '"id":487768939548536833,"id_str":"487768939548536833",'
+        test = test + '"text":"@mhvgovhacktest Show me a Charnwood sculpture"}]'
+        jsontest = json.loads(test)
+        self.assertEqual(t.parse_tweets(jsontest), 'Charnwood', "Tweet found")
 
 if __name__ == '__main__':
     unittest.main()
--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,27 +1,31 @@
-# import requests
+import re
 from requests_oauthlib import OAuth1Session
 import settings
 
 
 class Twitter(object):
-    # Reads the Twitter
-    list_of_burbs = ['Belconnen', 'Civic', 'Gungahlin',
-                     'North Canberra', 'Parliamentary Triangle',
-                     'South Canberra', 'Tuggeranong', 'Woden']
 
     def __init__(self):
-        self.api = 'https://api.twitter.com/1.1/statuses/mentions.json?include_entities=true'
-        self.access_token_url = 'https://api.twitter.com/oauth/access_token'
-        self.request_token_url = 'https://api.twitter.com/oauth/request_token'
+        self.suburbs = ['Belconnen', 'Civic', 'Gungahlin', 'Charnwood',
+                        'North Canberra', 'Parliamentary Triangle',
+                        'South Canberra', 'Tuggeranong', 'Woden']
+
+        self.url = 'https://api.twitter.com/1.1/statuses/mentions_timeline.json'
 
     def get_latest_suburb(self):
         return "Charnwood"  # todo, make this work
+
+    def parse_tweets(self, response):
+        for item in response:
+            for word in re.findall(r"[\w']+", item['text']):
+                if word in self.suburbs:
+                    return word
 
     def get_tweet(self):
         twitter_session = OAuth1Session(settings.TWITTER_CLIENT_KEY,
                                         client_secret=settings.TWITTER_CLIENT_SECRET,
                                         resource_owner_key=settings.TWITTER_RESOURCE_KEY,
                                         resource_owner_secret=settings.TWITTER_RESOURCE_SECRET)
-        print(twitter_session.fetch_request_token(self.request_token_url))
-        print(twitter_session.fetch_access_token(self.access_token_url))
-        pass
+        r = twitter_session.get(url="https://api.twitter.com/1.1/statuses/mentions_timeline.json")
+        if r.ok:
+            return self.parse_tweets(r.json())
