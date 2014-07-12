7e6cc57afd6dfdcf66d27aef5114efa4e2b60316
Max Bainrot <mbainrot@gmail.com>
2014-07-12 15:49:45 +1000

    Implemented script to fix the ACT Artworks json data from data.gov.au
    It adds a new key called "local_region" which is set to the original "suburb" and suburb is then set to the data that is resolved from the local database
    
    See wiki for further documentation on how to setup the postgis database

---
+++ Data/process_actart_data.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+# DEPENDANCIES
+# pip: psycopg2,urllib2,json
+# apt: postgresql-server-dev-9.1
+
+import psycopg2
+import json
+import urllib2
+
+# CONFIG
+targeturl = 'http://www.data.act.gov.au/resource/j746-krni.json'
+connection_string = 'dbname=abs_sa1 user=postgres'
+outputfile = 'output.json'
+
+print 'Retrieving JSON'
+opener = urllib2.build_opener()
+f = opener.open(targeturl)
+jres = json.load(f)
+
+newdict = []
+
+for item in jres:
+    # THIS CODE HERE ACTUALLY IS TO BE IN A LOOP!
+    lat = item['location']['latitude']
+    lon = item['location']['longitude']
+    coord = lon + ' ' + lat
+
+    blank = False
+
+    if blank is False:
+        print 'Searching database for ', coord
+        # coord = '149.234167 -35.353333'
+        sql = "SELECT * FROM sa1_2011_aust WHERE ST_Intersects(ST_GeomFromText('POINT("+coord+")'),the_geom) LIMIT 10;"
+        # ^---- We have to concat here because if we pass it via parameters it will automatically enclose in single quotes
+        conn = psycopg2.connect(connection_string)
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        rows = cur.fetchall()
+        conn.close()
+
+        item['local_region'] = item['suburb']
+
+        if len(rows) == 1:
+            item['suburb'] = rows[0][5]
+        else:
+            item['suburb'] = ''
+
+        newdict.append(item)
+
+f = open(outputfile, 'w')
+f.write(json.dumps(newdict))
+f.close()
