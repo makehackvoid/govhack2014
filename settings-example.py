f46bacb3298adf78e770e87e9b7b6da2004aa0a0
Jamie Reid <jamie@jre.id.au>
2014-07-13 12:32:41 +1000

    Update main page with new style.  Closes #96

--- settings-example.py
+++
@@ -1,8 +0,0 @@
-# rename to settings.py
-TWITTER_CLIENT_KEY = "xxxx"
-TWITTER_CLIENT_SECRET = "xxxx"
-TWITTER_RESOURCE_KEY = "xxxx"
-TWITTER_RESOURCE_SECRET = "xxxx"
-DATABASE = "test.db"
-DEBUG = True
-SECRET_KEY = "test"
